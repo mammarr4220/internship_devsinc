44e99f4afe73533f504f1adaa71797cca9450af8
Muhammad Ammar Rashid <mammarr4220@gmail.com>
2026-07-22 16:56:45 +0400

    Pushed a small Python conditionals program to branch1

---
+++ py_file.py
@@ -0,0 +1,10 @@
+def function1():
+    a = input("Enter a number: ")
+    b = input("Enter another number: ")
+    
+    if a > b:
+        print("The first number is greater.")
+    else:
+        print("The second number is greater.")
+
+function1()
