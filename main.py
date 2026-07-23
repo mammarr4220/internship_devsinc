d9cedae6b6759d9bfc09b04320088d64f2377842
Muhammad Ammar Rashid <mammarr4220@gmail.com>
2026-07-23 13:44:04 +0400

    Added a functionality that counts the number of tasks a user has to complete.

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ def main():
         task_input = input("Enter a task you need to do: ")
         
         if task_input == "":
+            task_count = len(list_of_tasks)
             print(list_of_tasks)
             print("No more tasks need to be listed.")
+            print("You have a total of", task_count, "tasks to complete.")
             break
         else:
             list_of_tasks.append(task_input)
             continue
-    
+
 main()
